c69a868b1ce7ccd7a5655f6787b1d2cdf7b626ee
MD ASIF RAZA <66127214+mdasifraza@users.noreply.github.com>
2022-12-03 13:52:44 +0000

    reduce pollyfill

--- index.js
+++ index.js
@@ -30,13 +30,24 @@ Array.prototype.newFilter = function(callbackFn) {
     return arr
 }
 
+Array.prototype.newReduce= function(callbackFn, initialValue) {
+  let accumulator = initialValue;
+	for (let i = 0; i < this.length; i++) {
+	    if (accumulator !== undefined) {
+	      accumulator = callbackFn.call(undefined, accumulator, this[i],   i, this);
+	    } else {
+	      accumulator = this[i];
+	    }
+  }
+  return accumulator;
+}
+
 const arr = [1,2,3,4]
 const temp = arr.map((item, i) => item * i+item)
 const temp1 = arr.newFilter((item, i) => item >= 3)
-
-arr.newMap((item, i, ar) => console.log(item, i, ar))
+const temp2 = arr.newReduce((accu, currValue) => accu+currValue, initialValue=0)
 
 console.log(temp)
 console.log(temp1)
-// console.log(temp)
+console.log(temp2)
 // console.log(temp)
